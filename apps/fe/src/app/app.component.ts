c94ca5a14f7106369c7b1c4d802f5e31af9fcd48
MichalWolanin <michalwolanin02@gmail.com>
2024-04-20 18:45:41 +0200

    Remove be projects and tailwindcss.

--- apps/fe/src/app/app.component.ts
+++ apps/fe/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
