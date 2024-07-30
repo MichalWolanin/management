cf688bee40ce8b6e73a00c56c2bd112fe90ebea9
MichalWolanin <michalwolanin02@gmail.com>
2024-07-31 01:35:40 +0200

    initial

---
+++ apps/fe/src/app/components/summary/summary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-summary',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './summary.component.html',
+  styleUrl: './summary.component.scss',
+})
+export class SummaryComponent {}
