7cd111292a119b88e426732ca1c4f4ae2e0643e4
js-003 <jakubsokal13@gmail.com>
2024-09-25 22:35:27 +0100

    initial commit

--- be/obj/Debug/net8.0/be.AssemblyInfo.cs
+++ be/obj/Debug/net8.0/be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c6b1154feaecaa624d61a383c99da53394da97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf1e24e791d753b71ce7698f251e57665cad0b10")]
 [assembly: System.Reflection.AssemblyProductAttribute("be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
